3e496dcd1babb05bdab469ed601c9ff01369bb82
Zakwan105 <khairulzakwan105@gmail.com>
2021-11-22 21:05:56 +0800

    php crud

--- simpan_kemaskini.php
+++ simpan_kemaskini.php
@@ -7,10 +7,10 @@ $ic = $_POST['ic'];
 
 $sql = "UPDATE senaraipelajar SET nama = ?, ic = ? WHERE idsenarai = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param('sii', $nama, $ic, $idsenarai);
+$stmt->bind_param('ssi', $nama, $ic, $idsenarai);
 $stmt->execute();
 
-if ($mysqli->error) {
+if ($conn->error) {
 ?>
     <script>
         alert('Maaf! Nama tersebut sudah wujud dalam senarai');
--- tambah.php
+++ tambah.php
@@ -1,29 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <title>Add</title>
+    <title>Add New</title>
 </head>
-
 <body>
-    <form method="post" action="simpan.php">
-        <div class="input-group">
-            <label>Nama </label>
-            <input type="text" name="nama" value="">
-        </div>
-        <div class="input-group">
-            <label>ic</label>
-            <input type="text" name="ic" value="">
-        </div>
-        <div class="input-group">
-            <button class="btn" type="submit" name="save">Simpan</button>
-        </div>
-    </form>
+<form method="post" action="simpan.php" >
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="nama" value="">
+		</div>
+		<div class="input-group">
+			<label>IC</label>
+			<input type="text" name="ic" value="">
+		</div>
+		<div class="input-group">
+			<button class="btn" type="submit" name="save" >Simpan</button>
+		</div>
+	</form>
 </body>
-
 </html>
